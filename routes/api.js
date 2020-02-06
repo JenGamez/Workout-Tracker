9d31f2a7e7fa0bc276f19587b3379edd4979f330
Jennifer Gamez <jenggamez@gmail.com>
2020-02-06 14:08:10 -0800

    added html and api routes, neither of which are working yet

--- routes/api.js
+++ routes/api.js
@@ -15,10 +15,42 @@ router.get("/api/workouts", (req, res) => {
   });
 
   // add new workout
-  router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+//   router.post("/api/workouts", ({ body }, res) => {
+//     Workout.create(body)
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// add new workout
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create({
+                type: req.body.type,
+                name: req.body.name,
+                duration: req.body.duration,
+                weight: req.body.weight,
+                reps: req.body.reps,
+                sets: req.body.sets,
+                distance: req.body.distance
+  })
+  .then(function(dbWorkout) {
+    res.json(dbWorkout);
+  });
+});
+
+
+  // View (GET) the combined eight of multiple exercise on the "stats" page
+  // Ask Chris about getWorkoutsInRange() on api.js
+
+
+  router.get("/api/workouts", (req, res) => {
+    console.log("Getting exercise info")
+    Workout.find({})
         .then(dbWorkout => {
-            res.json(dbWorkout);
+            res.json(dbWorkout)
         })
         .catch(err => {
             res.status(400).json(err);
@@ -26,11 +58,9 @@ router.get("/api/workouts", (req, res) => {
 });
 
 
-  
-  //add the exercise to the current workout
 
-
-  // update/add exercise to workout
+  //add the exercise to the previous workout - PUT- don't need to do this, but want to if have time
+// add new exercises to a new workout plan
 
   
 
--- routes/html.js
+++ routes/html.js
@@ -11,14 +11,17 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/exercisse", function (req, res) {
+app.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
-  app.get("/stats", function (req, res) {
+  app.get("/exercise?", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
-  
+
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });  
 };
 
 
